477bdbf10e56980d7181ad0b7838fa2bed719a03
tomporter518 <tomporterjr@aol.com>
2024-06-23 15:01:36 -0400

    Refactor to use config files

--- netatmo-wunderground-pws.js
+++ netatmo-wunderground-pws.js
@@ -2,7 +2,9 @@
 var netatmo = require('netatmo')
 var PWS = require('wunderground-pws');
 
-var netatmoAuth;
+const { readFile } = require('fs');
+
+var netatmoConfig;
 var wundergroundAuth;
 
 var netatmo_pws = function (args) {
@@ -10,16 +12,26 @@ var netatmo_pws = function (args) {
   };
 
 netatmo_pws.prototype.setAuthVars = function(args) {
-    netatmoAuth = {
-        "client_id": args.netatmo_client_id,
-        "client_secret": args.netatmo_client_secret,
-        "refresh_token": args.netatmo_refresh_token,
-        "timeout": args.netatmo_timeout
-    };
-    wundergroundAuth = {
-        "wundergroundStationId": args.wundergroundStationId,
-        "wundergroundUserPassword": args.wundergroundUserPassword
-    };
+    if(args) {
+        if(typeof args.netatmo === "string") {
+            netatmoConfig = args.netatmo;
+        } else {
+            console.error("Netatmo config filename missing");
+        }
+
+        if(typeof args.wunderground === 'string') {
+            readFile(args.wunderground, "utf8", (error, data) => {
+                if(error) {
+                    console.error("Wunderground config read error:" + error);
+                return;
+                } else if(data) {
+                  wundergroundAuth = JSON.parse(data);
+		}
+             });
+        } else {
+            console.error("Wunderground config filename missing");
+        }
+    }
 }
 
 var pws;
@@ -35,30 +47,35 @@ var tempf;
 var rainin;
 var dailyrainin;
 var baromin;
-var softwaretype=  'netatmo-wunderground-pws';
+var softwaretype = 'netatmo-wunderground-pws';
 
 //Get data from Netatmo weather station
-netatmo_pws.prototype.getNetatmoData = function (){
+netatmo_pws.prototype.getNetatmoData = function () {
     try {
-        api = new netatmo(netatmoAuth);
+        if(!api) {
+            api = new netatmo(netatmoConfig);
 
-        api.on("error", function(error) {
-            console.error('Netatmo threw an error: ' + error);
-        });
+            api.on("error", function(error) {
+                console.error('Netatmo threw an error: ' + error);
+            });
 
-        api.on("warning", function(error) {
-            console.log('Netatmo threw a warning: ' + error);
-        });
+            api.on("warning", function(warning) {
+                console.log('Netatmo threw a warning: ' + warning);
+            });
 
+            api.on("info", function(info) {
+                console.log('Netatmo logged info: ' + info);
+            });
+	}
 
         console.debug("Getting Netatmo data...");
         api.getStationsData(function(err, devices) {
             let dev = devices[0];
             baromin = dev.dashboard_data.Pressure * 0.0295299830714;
 
-            for (let mod of dev.modules){
-                if (mod.type == "NAModule1"){   //Outdoor module
-                    if (mod.reachable){
+            for (let mod of dev.modules) {
+                if (mod.type == "NAModule1") {   //Outdoor module
+                    if (mod.reachable) {
                         console.debug("Got outdoor data...");
                         let data = mod.dashboard_data;
                         tempf = convertFromCtoF(data.Temperature);
@@ -70,8 +87,8 @@ netatmo_pws.prototype.getNetatmoData = function (){
                         console.error("Outdoor module is unreachable.");
                     }
                 }
-                else if (mod.type == "NAModule3"){  //Rain module
-                    if (mod.reachable){
+                else if (mod.type == "NAModule3") {  //Rain module
+                    if (mod.reachable) {
                         console.debug("Got rain module data...");
                         let data = mod.dashboard_data;
                         rainin = convertFromMmtoIn(data.sum_rain_1);
@@ -81,8 +98,8 @@ netatmo_pws.prototype.getNetatmoData = function (){
                         console.error("Rain module is unreachable.");
                     }
                 }
-                else if (mod.type == "NAModule2"){  //Wind module
-                    if (mod.reachable){
+                else if (mod.type == "NAModule2") {  //Wind module
+                    if (mod.reachable) {
                         console.debug("Got wind module data...");
                         let data = mod.dashboard_data;
                         winddirection = data.WindAngle;
@@ -101,23 +118,23 @@ netatmo_pws.prototype.getNetatmoData = function (){
     }
 }
 
-function convertFromCtoF(value){
+function convertFromCtoF(value) {
     return value * 9 /5 + 32
 }
 
-function convertFromKphToMph(value){
+function convertFromKphToMph(value) {
     return value * 0.621371;
 }
 
-function convertFromMmtoIn(value){
+function convertFromMmtoIn(value) {
     return value * 0.0393701;
 }
 
 //Send to Wunderground
-function setObservations(){
+function setObservations() {
     try {
         pws = undefined;
-        pws = new PWS(wundergroundAuth.wundergroundStationId, wundergroundAuth.wundergroundUserPassword);
+        pws = new PWS(wundergroundAuth.stationId, wundergroundAuth.userPassword);
         console.debug("Sending to Weather Underground...");
         console.debug("Temp: " + tempf);
         console.debug("Humidity: " + humidity);
@@ -140,8 +157,8 @@ function setObservations(){
             softwaretype: softwaretype
         });
 
-        pws.sendObservations(function(err, success){
-            if (err){
+        pws.sendObservations(function(err, success) {
+            if (err) {
                 console.error("Error sending data to Weather Underground: " + err.message);
             }
             else{
